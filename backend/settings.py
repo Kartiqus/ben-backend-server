1b12c5e78419062d4bdb7d36faf8a3b2c60d9453
Kartiqus <vicaire.des.ombres@gmail.com>
2025-02-09 22:23:03 +0000

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -61,15 +61,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# Base de données
+# Base de données (SQLite)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
